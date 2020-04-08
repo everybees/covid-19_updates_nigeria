40bd9ca8b4ccd5e413f6b95a154c253589afb991
everybees <everybees@localhost.localdomain>
2020-04-08 21:55:40 +0100

    08/04/2020

--- scripts/script.js
+++ scripts/script.js
@@ -737,6 +737,21 @@ map.on('load', function() {
         });
     });
 
+    document.getElementById('delta').addEventListener('click', function() {
+        // Fly to a random location by offsetting the point
+        // by up to 5 degrees.
+        map.flyTo({
+            bearing: 27,
+            center: [5.680004434, 5.890427265],
+            duration: 3000,
+            zoom: 9.5,
+            pitch: 50,
+            speed: 0.6,
+            // style: 'mapbox://styles/mapbox/streets-v11',
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
+    });
+
     document.getElementById('katsina').addEventListener('click', function() {
         // Fly to a random location by offsetting the point
         // by up to 5 degrees.
